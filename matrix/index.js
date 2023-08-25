77a64206e1d18f3268291ead78b0b10aa4862fe6
Rodrigo Sakae <devsakae@gmail.com>
2023-08-25 11:03:45 -0300

    feat: skelethon of cyclotron working

---
+++ matrix/index.js
@@ -0,0 +1,60 @@
+// We want to circulate a particle inside a cyclotron. The particles used are: e, p, n (electron, proton, and neutron). Each particle has a circulation function within the cyclotron, which can create either a closed or an open cycle.
+
+// Find the algorithm that satisfies the following cases for each particle:
+
+// Input
+// [particle, matrix] ~> Example: cyclotron(“e”, matrix)
+
+function cyclotron(params) {
+	// Check if params exists, and is an array.
+	if (!params || !Array.isArray(params)) return 'ERROR: Use cyclotron([particle, matrix])';
+	
+	// Check if informed params are valid.
+  let particles = ['e', 'n', 'p']; // Valid particles
+  const particle = params.length > 1 ? params[0] : null;
+  const matrix = params.length > 1 ? params[1] : params[0];
+  if (particle && (typeof particle !== 'string' || !particles.includes(particle))) return 'ERROR: Please inform valid particle';
+  if ((matrix && typeof matrix !== 'number') || matrix < 1) return 'ERROR: Please inform matrix number higher than 0';
+
+	// Prepares for response.
+	const arrayRow = new Array(matrix).fill(1);
+	
+	// If particle is informed (and valid)
+	let response = [];
+	switch (particle) {
+		case 'e': // Electron
+
+			response = 'Electron';
+			break;
+
+		case 'n': // Neutron
+			response = 'Neutron'
+			break;
+
+		case 'p': // Proton
+			response = 'Proton';
+			break;
+
+		default: // Particle not informed
+			for (let i = 1; i <= matrix; i++) response.push(arrayRow);
+			break;
+		}
+
+		return response;
+}
+
+
+// Params test
+console.log(cyclotron()); 											// Expects "ERROR: Use cyclotron([particle, matrix])"
+console.log(cyclotron({})); 										// Expects "ERROR: Use cyclotron([particle, matrix])"
+console.log(cyclotron('e', 3)); 								// Expects "ERROR: Use cyclotron([particle, matrix])"
+console.log(cyclotron(['not a particle', 4])); 	// Expects "ERROR: Please inform valid particle"
+console.log(cyclotron([5, 5])); 								// Expects "ERROR: Please inform valid particle"
+console.log(cyclotron([['e'], 6])); 						// Expects "ERROR: Please inform valid particle"
+console.log(cyclotron(['e', 0])); 							// Expects "ERROR: Please inform matrix number higher than 0"
+console.log(cyclotron(['e', 'x'])); 						// Expects "ERROR: Please inform matrix number higher than 0"
+console.log(cyclotron(['x'])); 									// Expects "ERROR: Please inform matrix number higher than 0"
+
+// Content tests
+console.log(cyclotron([5])); 										// Expects "No particle answer"
+console.log(cyclotron(['p', 5])); 							// Expects "Particle asnswer"
