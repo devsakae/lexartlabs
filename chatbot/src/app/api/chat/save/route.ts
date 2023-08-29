5c7f1b7965b7361aa6b52987f2fcd22819d4f497
Rodrigo Sakae <devsakae@gmail.com>
2023-08-29 10:45:48 -0300

    refactor: /api/save saving on json file

--- chatbot/src/app/api/chat/save/route.ts
+++ chatbot/src/app/api/chat/save/route.ts
@@ -1,87 +1,23 @@
-// import { NextRequest, NextResponse } from "next/server";
-// const path = require('path');
-// const fs = require('fs');
-
-// export async function POST(req: NextRequest) {
-//   // fs.mkdir('./history', { recursive: true }, (err: any) => {
-//   //   if (err) throw err;
-//   // });
-//   const res = await req.json();
-//   // fs.mkdir(path.join(__dirname, '/history'))
-//   // const dirpath = path.join(__dirname, '/history');
-//   fs.writeFile(path + '/relatorio.json', JSON.stringify(res), 'utf8', function (err: Error){
-//     if (err) console.error('Some error occured')
-//     else console.log('Written!');
-//   })
-//   return NextResponse.json({ status: 200, message: 'LOG_SAVED' });
-// }
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// }
-
-// import { NextApiHandler, NextApiRequest } from "next";
-import { NextRequest, NextResponse } from 'next/server';
-import fs from "fs";
-import { stringify } from "csv-stringify";
-// import { firestore } from "../../firestore";
+import { NextRequest, NextResponse } from "next/server";
+const path = require('path');
+const fs = require('fs');
 
 export async function POST(req: NextRequest) {
-  const { name, log } = await req.json();
-  const filename = `Chat with ${name}.csv`;
-  console.log('creating a chat file named', filename);
-  const writableStream = fs.createWriteStream(filename);
-  const columns = ["timestamp", "author", "message"];
-  const stringifier = stringify({ header: true, columns });
+  const body = await req.json();
 
-  stringifier.pipe(writableStream);
-  stringifier.on('finish', async () => {
-    const csvFile = await fs.promises.readFile(
-      `${process.cwd()}/${filename}`,
-      "utf-8"
-    );
-    return NextResponse.json({
-      status: 200,
-      send: csvFile
-    })
-  });
-  for (const entry of log) {
-    console.log('writing', entry.author, 'message')
-    stringifier.write([entry.timestamp, entry.author, entry.message], "utf-8");
-  }
-  stringifier.end();
-
-    // const csvFile = await fs.promises.readFile(
-    //   `${process.cwd()}/${filename}`,
-    //   "utf-8"
-    // );
-    // return NextResponse.json({
-    //   status: 200,
-    //   send: csvFile
-    // header: {
-    //   'Content-Type': 'text/csv',
-    //   'Content-Disposition': `attachment; filename=${csvFile}`,
-    // },
-    // message: 'Log saved'
-    // })
-
-    // stringifier.on("close", async () => {
-    //   const csvFile = fs.readFileSync(`${process.cwd()}/${filename}`, "utf-8");
-    //   return NextResponse.json({ status: 200, message: 'LOG_SAVED' });
-    // res
-    //   .status(200)
-    //   .setHeader("Content-Type", "text/csv")
-    //   .setHeader("Content-Disposition", `attachment; filename=${filename}`)
-    //   .send(csvFile);
-    // });
-    // stringifier.end();
-    // res.status(200).setHeader("Content-Type", "text/csv").send({ message: 'This is a test '});
+  for (const entry of body.log) {
+    fs.appendFileSync(`${__dirname}/history_${body.name}.json`, JSON.stringify(entry));
   };
+  
+  // fs.writeFile(path + '/relatorio.json', JSON.stringify(res), 'utf8', function (err: Error){
+  //   if (err) console.error('Some error occured')
+  //   else console.log('Written!');
+  // })
+  return NextResponse.json({ status: 200, message: 'LOG_SAVED' });
+}
 
-  export const config = {
-    api: {
-      bodyParser: false,
-    },
-  }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
